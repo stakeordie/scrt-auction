c3e8b3a06adff959c94a02f50018f27a156096f7
Victor Valle <victor.valle@lkmx.io>
2020-12-22 19:00:26 -0800

    First commit

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <simple-layout>
+    <template #header>
+      <simple-header>
+        <img class="header__logo" src="@/assets/flare--white.png">
+        <nav>
+          <g-link to="/">Index</g-link>
+          <g-link to="/about">About</g-link>
+        </nav>
+      </simple-header>
+    </template>
+
+    <page>
+
+      <slot></slot>
+
+    </page>
+
+    <template #footer>
+      <simple-footer>
+        <p>Flare Project</p>
+      </simple-footer>
+    </template>
+
+  </simple-layout>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style lang="scss">
+.header {
+  &__logo {
+    height: 60px;
+    padding: var(--f-gutter-s) var(--f-gutter);
+  }
+}
+</style>
---
+++ src/templates/Content.vue
@@ -0,0 +1,15 @@
+<template>
+  <default-layout>
+    <VueRemarkContent />
+  </default-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
