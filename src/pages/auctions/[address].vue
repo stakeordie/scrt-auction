362cfb049b1f951dbf9995e1fedc48bebf70483d
Sandy Corsillo <the_dusky@hey.com>
2020-12-31 12:09:35 -0500

    address.vue page

---
+++ src/pages/auctions/[address].vue
@@ -0,0 +1,38 @@
+<template>
+  <default-layout>
+    <page>
+        <columns>
+        <h1>Secret Auction</h1>
+        <block>
+          {{ address }}
+        </block>
+        <block v-if="auctionInfo">
+          <div>Sell Token: {{ auctionInfo.auction_info.sell_token.token_info.symbol }} ({{auctionInfo.auction_info.sell_amount}})</div>
+          <div>Bid Token: {{ auctionInfo.auction_info.bid_token.token_info.symbol }}</div>
+        </block>
+        </columns>
+    </page>
+  </default-layout>
+</template>
+
+<script>
+import DefaultLayout from "../../layouts/DefaultLayout.vue";
+export default {
+  components: { DefaultLayout },
+  data() {
+    return {
+      address: "",
+      auctionInfo: null,
+      codeHash: ""
+    };
+  },
+  async created() {
+    this.address = this.$route.params.address;
+    this.auctionInfo = await this.$scrtjs.queryContract(this.address, {"auction_info":{}})
+    this.codeHash = await this.$scrtjs.getContractHash(this.address);
+    console.log(this.codeHash);
+  },
+};
+</script>
+
+<style></style>
