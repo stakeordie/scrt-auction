a50d173ba3e144a5645346753bc2a4017b89e476
Sandy Corsillo <the_dusky@hey.com>
2021-01-28 15:07:56 -0500

    mainnet tokens added

--- src/main.js
+++ src/main.js
@@ -111,9 +111,168 @@ export default function(Vue, { router, head, isClient }) {
         name: "ssecret",
         label: "sscrt",
         decimals: 6
+      },
+      {
+        codeId: 10,
+        symbol: "SETH",
+        address: "secret1wuzzjsdhthpvuyeeyhfq2ftsn3mvwf9rxy6ykw",
+        name: "Secret Ethereum",
+        label: "secret-eth",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SUSDT",
+        address: "secret18wpjn83dayu4meu6wnn29khfkwdxs7kyrz9c8f",
+        name: "ecret Tether",
+        label: "secret-usdt",
+        decimals: 6
+      },
+      {
+        codeId: 10,
+        symbol: "SDAI",
+        address: "secret1vnjck36ld45apf8u4fedxd5zy7f5l92y3w5qwq",
+        name: "Secret Dai Stablecoin",
+        label: "secret-dai",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SCOMP",
+        address: "secret1szqzgpl9w42kekla57609y6dq2r39nf0ncx400",
+        name: "Secret Compound",
+        label: "secret-comp",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SUNI",
+        address: "secret1ds8850j99cf5h3hygy25f0zzs6r6s7vsgfs8te",
+        name: "Secret Uniswap",
+        label: "secret-uni",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SYFI",
+        address: "secret15grq8y54tvc24j8hf8chunsdcr84fd3d30fvqv",
+        name: "Secret yearn.finance",
+        label: "secret-yfi",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "STUSD",
+        address: "secret1ryh523y4e3233hphrkdslegszqz8syjfpthcpp",
+        name: "Secret TrueUSD",
+        label: "secret-tusd",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SOCEAN",
+        address: "secret12sjaw9wutn39cc5wqxfmkypm4n7tcerwfpvmps",
+        name: "Secret Ocean Token",
+        label: "secret-ocean",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SLINK",
+        address: "secret1xcrf2vvxcz8dhtgzgsd0zmzlf9g320ea2rhdjw",
+        name: "Secret ChainLink Token",
+        label: "secret-link",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SMKR",
+        address: "secret1tqltnm8f53xnprmnlurczf6sr86a4mgztafxzp",
+        name: "Secret Maker",
+        label: "secret-maker",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SSNX",
+        address: "secret15c5ftq4rq7xq3tml4nphv2fvz3u7kg73a583qp",
+        name: "Secret Synthetix Network",
+        label: "secret-snx",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SAAVE",
+        address: "secret1yxwnyk8htvvq25x2z87yj0r5tqpev452fk6h5h",
+        name: "Secret Aave Token",
+        label: "secret-aave",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SBAND",
+        address: "secret1p4zvqgxggrrk435nj94p6la2g4xd0rwssgzpsr",
+        name: "Secret BandToken",
+        label: "secret-band",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SKNC",
+        address: "secret1rs389ss2jch4xjmxv5guru86s8y839nmjsrm5d",
+        name: "Secret KyberNetwork",
+        label: "secret-knc",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SWBTC",
+        address: "secret1g7jfnxmxkjgqdts9wlmn238mrzxz5r92zwqv4a",
+        name: "Secret Wrapped BTC",
+        label: "secret-wbtc",
+        decimals: 8
+      },
+      {
+        codeId: 10,
+        symbol: "SBAC",
+        address: "secret1ezg8weaamcr99848qhkqcf2ap5xz7nwe3cy22x",
+        name: "Secret Basis Cash",
+        label: "secret-bac",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SSUSHI",
+        address: "secret19uje5xy80rm6rfu03df2xea532mcalw9hv8vf9",
+        name: "Secret Sushi",
+        label: "secret-sushi",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SRSR",
+        address: "secret1vcm525c3gd9g5ggfqg7d20xcjnmcc8shh902un",
+        name: "Secret ReserveRights",
+        label: "secret-rsr",
+        decimals: 18
+      },
+      {
+        codeId: 10,
+        symbol: "SUSDC",
+        address: "secret1h6z05y90gwm4sqxzhz4pkyp36cna9xtp7q0urv",
+        name: "Secret USDC",
+        label: "secret-usdc",
+        decimals: 6
+      },
+      {
+        codeId: 10,
+        symbol: "SDPI",
+        address: "secret1ukn328k6y3th5nw9z00p8lvk5s7m05cr9p06cu",
+        name: "Secret DefiPulseIndex",
+        label: "secret-dpi",
+        decimals: 18
       }
     ]
-
   } else {
     availableTokens = [
       {
