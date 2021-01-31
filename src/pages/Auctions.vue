f3591e350dcfbf8434312174d3594858ccb2e732
Victor Valle <victor.valle@lkmx.io>
2021-01-31 14:29:47 -0800

    Moved new page to safe location for redirects

--- src/pages/Auctions.vue
+++ src/pages/Auctions.vue
@@ -4,7 +4,7 @@
       <!-- Auctions header -->
       <div class="auctions-header">
         <h1>Auctions</h1>
-        <router-link :to="'/auctions/new'" class="button">Create auction</router-link>
+        <router-link :to="'/new'" class="button">Create auction</router-link>
           <!--button @click="createViewingKey()">Create Viewing Key</button-->
           <!--button @click="listUserAuctions()">ListUserAuctions</button-->
       </div>
