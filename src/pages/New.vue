66bb10992e694efa62e1dfd53bb5961467f09fc1
Sandy Corsillo <the_dusky@hey.com>
2021-03-13 12:09:31 -0500

    custom sell token form

--- src/pages/New.vue
+++ src/pages/New.vue
@@ -36,6 +36,40 @@
                                 </select>
                             </validation-provider>
 
+                            <!-- START - Custom Sell Token Section -->
+                                <div class="custom-token" v-if="auctionForm.sellToken == 'customSellToken'">
+                                    <h3 class="custom-token__title">Enter Custom Token Details</h3>
+                                    <div class="custom-token__label-search">
+                                        <label for="custom-sell-token-label-search">Find Address by Label</label>
+                                        <span class="error">{{ errors[0] }}</span>
+                                        <input name="custom-sell-token-label-search" type="text" v-model.trim="auctionForm.customSellTokenLabelSearch" />
+                                    </div>
+                                    <button type="button" class="custom-token__label-search-button" @click="getContractAddressFromLabel">Search</button>
+                                    <validation-provider class="custom-token__contract_address" :rules="`required`" v-slot="{ errors }">
+                                        <label for="custom-sell-token-contract-address">Contract Address</label>
+                                        <span class="error">{{ errors[0] }}</span>
+                                        <input name="custom-sell-token-contract-address" type="text" v-model.trim="auctionForm.customSellTokenContractAddress" @change="getCustomSellToken"/>
+                                    </validation-provider>
+                                    <div class="custom-token__name">
+                                        <label for="sell-amount">Name</label>
+                                        <span class="error">{{ errors[0] }}</span>
+                                        <input name="sell-amount" type="text" readonly v-model.trim="auctionForm.customSellTokenName" />
+                                    </div>
+                                    <div class="custom-token__symbol">
+                                        <label for="sell-amount">Symbol</label>
+                                        <span class="error">{{ errors[0] }}</span>
+                                        <input name="sell-amount" type="text" readonly v-model.trim="auctionForm.customSellTokenSymbol" />
+                                    </div>
+                                    <div class="custom-token__decimals">
+                                        <label for="sell-amount">Decimals</label>
+                                        <span class="error">{{ errors[0] }}</span>
+                                        <input name="sell-amount" type="text" readonly v-model.trim="auctionForm.customSellTokenDecimals" />
+                                    </div>
+                                </div>
+
+                            <!-- END - Custom Sell Token Section -->
+
+
                             <!-- Bid token -->
                             <div class="auction-form__label-only">
                                 <label>Enter either a price or a minimum bid</label>
@@ -293,6 +327,12 @@ export default {
                 this.auctionForm.label = "auction-" + Math.round(Math.random() * 10000000);
             }
         },
+        async getContractAddressFromLabel() {
+            return null;            
+        },
+        async getCustomSellToken() {
+            return null;
+        },
         async increaseAllowance() {
             this.stage = "allowance--creating";
             this.allowanceError = "";
@@ -345,6 +385,50 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+// START - CUSTOM TOKEN
+
+.custom-token {
+    grid-column: 1 / 5;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    padding: 20px 20px 10px;
+    border: 3px solid white;
+    border-radius: 20px;
+    margin: 10px 0 25px 30px;
+    column-gap: var(--f-gutter);
+    align-items: end;
+
+    &__title {
+        grid-column: 1 / 4;
+    }
+
+    &__label-search {
+        grid-column: 1 / 3;
+    }
+
+    &__label-button {
+        grid-column: 3 / 4;
+    }
+
+    &__contract_address {
+        grid-column: 1 / 4;
+    }
+    &__name {
+        grid-column: 1 / 4;
+    }
+    &__symbol {
+        grid-column: 1 / 4;
+    }
+    &__decimals {
+        grid-column: 1 / 4;
+    }
+    &__submit-button {
+        grid-column: 1 / 4;
+    }
+}
+
+// END - CUSTOM TOKEN
 
 .auction-form {
     display: grid;
