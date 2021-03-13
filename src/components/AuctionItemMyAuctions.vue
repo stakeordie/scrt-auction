1d526774d95527af48a6a5677ed5b3ddf86ca9ad
Walter Hurtado <walter.hurtado@lkmx.io>
2021-03-12 18:17:47 -0800

    - Added new component 'AutionItemMyAuctions' - Redesign of 'User.vue'

---
+++ src/components/AuctionItemMyAuctions.vue
@@ -0,0 +1,172 @@
+<template>
+  <g-link :to="to" class="auction" v-if="auction"
+    :class="['theme-' + auction.color, 'status-' + auction.status.toLowerCase()]">
+    <p class="auction__pair">
+      <span class="sell-denom">{{ auction.sell.denom }}</span>
+      <span v-if="auction.bid">
+        /
+        <span class="bid-denom">{{ auction.bid.denom }}</span>
+      </span>
+    </p>
+
+    <div class="auction__bid-price">
+      <dl class="auction__bid" v-if="auction.bid && auction.price">
+        <dd class="auction__asking-price">
+          <token-amount :amount="auction.price" :decimals="auction.bid.decimals" :denom="auction.bid.denom">
+          </token-amount>
+          <span v-if="auction.sell.decimalAmount != 1"> per token </span><br><span
+            v-if="auction.sell.decimalAmount != 1"><span>({{ auction.bid.decimalMinimum}}
+              {{auction.bid.denom}})</span></span>
+        </dd>
+      </dl>
+      <dl v-if="auction.bid && auction.bid.winner">
+        <dt>Winning bid</dt>
+        <dd class="auction__winner">
+          <token-amount :amount="auction.bid.decimalWinner" :decimals="auction.bid.decimals" :denom="auction.bid.denom">
+          </token-amount>
+        </dd>
+      </dl>
+    </div>
+
+    <dl class="auction__sell">
+      <dt>For Sale</dt>
+      <dd>
+        <token-amount :amount="auction.sell.decimalAmount" :decimals="auction.sell.decimals"
+          :denom="auction.sell.denom"></token-amount>
+      </dd>
+    </dl>
+
+    <dl v-if="auction.endsAt && !auction.closedAt">
+      <dt>Target close</dt>
+      <dd :class="isEnded ? 'ended': ''">{{ endsAt }}</dd>
+    </dl>
+
+    <div v-if="auction.closedAt">
+      <dl>
+        <dt>Closed at</dt>
+        <dd>{{ closedAt }}</dd>
+      </dl>
+    </div>
+
+    <div class="auction__actions">
+      <g-image src="@/assets/icon-actions.svg"></g-image>
+    </div>
+
+    <!-- <div class="auction__status">
+      <div class="auction__status--status" :class="['auction__status--' + auction.status.toLowerCase()]">{{ auction.status }}</div>
+      <div class="auction__status--user-status auction__status--user-seller" v-if="auction.viewerIsSeller">You are the auction owner</div>
+      <div class="auction__status--user-status auction__status--user-bid" v-if="auction.viewerIsBidder && auction.viewerIsSeller">and you've placed a bid.</div>
+      <div class="auction__status--user-status auction__status--user-bid" v-else-if="auction.viewerIsBidder">You've placed a bid.</div>
+      <div class="auction__status--user-status auction__status--user-sold" v-if="auction.viewerWasSeller">You were the auction owner</div>
+      <div class="auction__status--user-status auction__status--user-won" v-if="auction.viewerIsWinner && auction.viewerWasSeller">and you won it.</div>     
+      <div class="auction__status--user-status auction__status--user-won" v-else-if="auction.viewerIsWinner">You won the auction</div>
+    </div> -->
+
+  </g-link>
+</template>
+
+<script>
+  import TokenAmount from "./TokenAmount.vue"
+
+  import moment from 'moment'
+
+  export default {
+    components: {
+      TokenAmount
+    },
+    props: ["auction", "to"],
+    computed: {
+      endsAt() {
+        return moment(this.auction.endsAt).format("YYYY-MM-DD HH:mm:ss");
+      },
+      closedAt() {
+        return moment(this.auction.closedAt).format("YYYY-MM-DD HH:mm:ss");
+      },
+      isEnded() {
+        return moment(this.auction.endsAt).isBefore();
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "@lkmx/flare/src/functions/respond-to";
+
+  .auction {
+    background-color: var(--color-black);
+
+    * {
+      font-size: 14px;
+      line-height: 20px;
+    }
+
+    &:hover {
+
+
+      border-color: var(--theme-washed-color);
+
+
+    }
+
+
+    border: 1px solid #2E323C;
+    background-color: #0D1017;
+
+
+    text-decoration: none;
+    padding: var(--f-gutter);
+    border-radius: 0px;
+    position: relative;
+    transition: background-color 0.7s;
+
+    &:not(.grid):not(.list) {
+      display: none;
+    }
+
+    // Layout specific
+    &.grid {
+      display: grid;
+      gap: var(--f-gutter);
+      background: red;
+    }
+
+    &.list {
+      display: grid;
+      gap: var(--f-gutter);
+      align-items: center;
+
+      @include respond-to("<=s") {
+        grid-template-columns: repeat(2, 1fr);
+      }
+
+      @include respond-to(">=m") {
+        grid-template-columns: repeat(5, 1fr);
+      }
+
+      dd {
+        margin-bottom: 0;
+        font-weight: normal;
+      }
+
+      .auction {
+        &__pair {
+          margin-bottom: 0;
+        }
+
+        &__bid-action {
+          opacity: 0.3;
+        }
+
+        &__status {
+          text-align: right;
+        }
+
+        &__actions {
+          justify-self: end;
+          padding-right: var(--f-gutter);
+        }
+      }
+    }
+
+  }
+</style>
