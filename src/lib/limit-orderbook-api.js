2b85811b7d3e24cddd96a04c7aa5d0ce6a55948b
Sandy Corsillo <the_dusky@hey.com>
2021-03-22 19:06:23 -0400

    limit-orderbook-api

--- src/lib/limit-orderbook-api.js
+++ src/lib/limit-orderbook-api.js
@@ -3,7 +3,7 @@ import statePersist from '../plugins/state-persist.js';
 //any function definitions
 
 export class LimitOrderbookApi {
-    
+
     factoryAddress
     //other addresses
     scrtClient
@@ -13,5 +13,30 @@ export class LimitOrderbookApi {
         this.factoryAddress = factoryAddress;
         //set other address
     }
+
+    // TODO: #64 Find out what fees will be and set them
+    getFees(txName) {
+        let feesObj = {};
+        let gas = "1000000";
+        let type = "exec";
+        let amount = "0";
+        let denom = "uscrt";
+        switch(txName) {
+
+            default:
+        }
+        if(amount == "0") {
+            amount = gas;
+        }
+        
+        feesObj[type] = {
+            amount: [{amount, denom}],
+            gas
+        };
+
+        return feesObj;
+    }
+
+    
 
 }
