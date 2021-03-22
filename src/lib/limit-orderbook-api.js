161a8d8f52e186650dc3cb3f696a5bb4c3294efb
Sandy Corsillo <the_dusky@hey.com>
2021-03-22 18:12:26 -0400

    LimitOrderbookApi Object

--- src/lib/limit-orderbook-api.js
+++ src/lib/limit-orderbook-api.js
@@ -3,13 +3,15 @@ import statePersist from '../plugins/state-persist.js';
 //any function definitions
 
 export class LimitOrderbookApi {
+    
     factoryAddress
     //other addresses
     scrtClient
-    
+
     constructor(scrtClient, factoryAddress /*, otherAddress*/) {
         this.scrtClient = scrtClient;
         this.factoryAddress = factoryAddress;
         //set other address
     }
+
 }
