c42410b1e6a9d6d766070ae31b9a9afe73ed6e76
Sandy Corsillo <the_dusky@hey.com>
2021-03-22 20:34:25 -0400

    added TODO to TokenSelector

--- src/components/TokenSelector.vue
+++ src/components/TokenSelector.vue
@@ -2,6 +2,7 @@
     <div class="custom-select" :tabindex="tabindex">
         <!-- Place below in the element above -->
         <!-- @blur="open = false" -->
+        <!-- TODO: #69 Token Selector background is too dark, should match complete auction steps -->
         <div class="selected" :class="{ open: open }" @click="open = !open">
             <g-image :immediate="true"
                 :src="require(`!!assets-loader?width=15&height=15!@/assets/token-icons/${selected.iconImg ? selected.iconImg : 'secret-scrt-logo.svg'}`)">
